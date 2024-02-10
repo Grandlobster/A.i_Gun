f6164625c412347fb083a32c81be2f785e6f04ff
Aadesh <118823460+Grandlobster@users.noreply.github.com>
2024-02-10 16:30:18 +0530

    Create Cordi_file.py
    Study of coordinates of Servo motors 
    Before that we need to decide what type of servo motors we need to buy

---
+++ Servo/Cordi_file.py
@@ -0,0 +1,7 @@
+import serial
+ser = serial.Serial("COM11", '9600', timeout=2)
+Xposition = 90
+Yposition = 90
+while True:
+  .... some code....
+#........................................................................ somebody study coordinates .................................................
